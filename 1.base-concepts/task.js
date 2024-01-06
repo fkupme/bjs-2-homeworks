ef79205bc6a73e87aaa3a361032383a27dd495a5
V. Kirichenko <fkupme@yandex.ru>
2024-01-06 19:57:34 +0300

    The first task is completed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,24 @@
 "use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+const solveEquation = (a, b, c) => {
+	let arr = [];
+	let d = Number(b ** 2 - 4 * a * c);
+	if (d > 0) {
+		arr.push((-b + Math.sqrt(d)) / (2 * a));
+		arr.push((-b - Math.sqrt(d)) / (2 * a))
+	} else if (d === 0) {
+		arr.push(-b / (2 * a))
+	};
+	return arr;
 }
-
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+console.log(solveEquation(3, -5, -2));
+console.log(solveEquation(4, -4, 1));
+console.log(solveEquation(1, -2, 3));
+const calculateTotalMortgage = (percent, contribution, amount, countMonths) => {
+	
+	const percentToMultiplier = percent /100
+	const monthMultiplier = percentToMultiplier / 12
+	const loanBody = amount - contribution
+	const MonthlyPayment = loanBody*(monthMultiplier +(monthMultiplier/(((1 + monthMultiplier)**countMonths)-1)))
+	const total = MonthlyPayment * countMonths
+	return total.toFixed(2)
 }
