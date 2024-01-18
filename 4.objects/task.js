160f974f5cf3f83343b14e44a3cf9ea3c8141e0b
V. Kirichenko <fkupme@yandex.ru>
2024-01-18 18:13:10 +0300

    task completed

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,7 +1,9 @@
+"use strict"
 function Student(name, gender, age) {
 	this.name = name;
 	this.gender = gender;
 	this.age = age;
+	this.marks = []
 }
 
 Student.prototype.setSubject = function (subjectName) {
@@ -9,11 +11,11 @@ Student.prototype.setSubject = function (subjectName) {
 }
 
 Student.prototype.addMarks = function (...marks) {
-	marks.length ? this.marks = marks : null;
+	return this.marks ? (this.marks.length? this.marks.push(...marks) : this.marks = marks): null;
 }
 
 Student.prototype.getAverage = function () {
-	this.hasOwnProperty('marks') ? this.Average = this.marks.reduce((acc, val) => acc + val, 0) / this.marks.length : null;
+	return this.marks && this.marks.length ? this.Average = this.marks.reduce((acc, val) => acc + val, 0) / this.marks.length : this.Average = 0;
 }
 
 Student.prototype.exclude = function (reason) {
@@ -23,20 +25,26 @@ Student.prototype.exclude = function (reason) {
 	this.excluded = reason
 	}
 }
+Student.prototype.hasMarks = function () {
+	return this.marks? true : false
+}
 
 
 
 let vlad = new Student('vlad', 'male', 26)
 vlad.setSubject('math')
-vlad.addMarks(2, 3, 5)
+vlad.addMarks()
+vlad.addMarks()
+
 vlad.getAverage()
 vlad.exclude()
+console.log(vlad.hasMarks())
 console.log(vlad);
 
 let student1 = new Student("Василиса", "женский", 19);
 student1.setSubject("Algebra");
 console.log(student1.getAverage()); // 0
-student1.addMarks(4, 5, 4, 5);
+student1.addMarks(5, 5, 5);
 console.log(student1.getAverage()); // 4.5
 console.log(student1);
 // {age: 19, gender: "женский", marks: [4, 5, 4, 5], name: "Василиса", subject: "Algebra"}
@@ -45,5 +53,9 @@ console.log(student1);
 let student2 = new Student("Артём", "мужской", 25);
 student2.setSubject("Geometry");
 student2.exclude('плохая учёба')
+student2.addMarks(5, 5, 5);
+student2.getAverage();
+
+console.log(student2.hasMarks())
 console.log(student2)
 // {name: "Артём", gender: "мужской", age: 25, excluded: "плохая учёба"}
