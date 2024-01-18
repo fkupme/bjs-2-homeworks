afd2c9da065d9e32475db16f13ec83d22c107b50
V. Kirichenko <fkupme@yandex.ru>
2024-01-18 18:13:26 +0300

    task completed

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -25,37 +25,3 @@ Student.prototype.exclude = function (reason) {
 	this.excluded = reason
 	}
 }
-Student.prototype.hasMarks = function () {
-	return this.marks? true : false
-}
-
-
-
-let vlad = new Student('vlad', 'male', 26)
-vlad.setSubject('math')
-vlad.addMarks()
-vlad.addMarks()
-
-vlad.getAverage()
-vlad.exclude()
-console.log(vlad.hasMarks())
-console.log(vlad);
-
-let student1 = new Student("Василиса", "женский", 19);
-student1.setSubject("Algebra");
-console.log(student1.getAverage()); // 0
-student1.addMarks(5, 5, 5);
-console.log(student1.getAverage()); // 4.5
-console.log(student1);
-// {age: 19, gender: "женский", marks: [4, 5, 4, 5], name: "Василиса", subject: "Algebra"}
-
-
-let student2 = new Student("Артём", "мужской", 25);
-student2.setSubject("Geometry");
-student2.exclude('плохая учёба')
-student2.addMarks(5, 5, 5);
-student2.getAverage();
-
-console.log(student2.hasMarks())
-console.log(student2)
-// {name: "Артём", gender: "мужской", age: 25, excluded: "плохая учёба"}
