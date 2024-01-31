9347c0dcffa013f4f03e7723428ef51aae1784a9
V. Kirichenko <fkupme@yandex.ru>
2024-01-31 14:34:28 +0300

    objects update

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -11,11 +11,13 @@ Student.prototype.setSubject = function (subjectName) {
 }
 
 Student.prototype.addMarks = function (...marks) {
-	return this.marks ? (this.marks.length? this.marks.push(...marks) : this.marks = marks): null;
+	if(this.marks){
+		return this.marks.length? this.marks.push(...marks) : this.marks = marks
+	}
 }
 
 Student.prototype.getAverage = function () {
-	return this.marks && this.marks.length ? this.Average = this.marks.reduce((acc, val) => acc + val, 0) / this.marks.length : this.Average = 0;
+	return this.marks && this.marks.length ? this.marks.reduce((acc, val) => acc + val, 0) / this.marks.length : 0;
 }
 
 Student.prototype.exclude = function (reason) {
