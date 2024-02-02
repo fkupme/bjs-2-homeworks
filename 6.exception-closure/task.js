a1a21c923d7c67402c70ac6a40bf3bc10518e4e6
V. Kirichenko <fkupme@yandex.ru>
2024-02-02 13:07:29 +0300

    exception-closure added

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,55 @@
-﻿
+﻿const parseCount = (val) => {
+	if(Number.isNaN(Number.parseFloat(val))) {
+		throw new Error('Невалидное значение')
+	} 
+	return Number.parseFloat(val)
+}
+const validateCount = (val) => {
+	try {
+		return parseCount(val)
+	} catch (error) {
+		return error
+	}
+}
+
+class Triangle{
+	constructor(a, b, c) {
+		if(a + b <= c || b + c <= a || c + a <= b) {
+			throw new Error('Треугольник с такими сторонами не существует')
+		}else{
+		this.a = a
+		this.b = b
+		this.c = c
+	}
+	}
+	get perimeter () {
+		return this.a + this.b + this.c
+		
+	}
+	get area () {
+		const p = this.perimeter / 2
+		return +(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3))
+	}
+
+
+}
+
+class ErrorTriangle {
+	constructor(){
+		
+	}
+	get perimeter () {
+		return 'Ошибка! Треугольник не существует'
+	}
+	get area () {
+		return 'Ошибка! Треугольник не существует'
+	}
+}
+
+const ErrorTriangle =(a, b, c) =>{
+	try {
+		return new Triangle(a, b, c)
+	} catch (error) {
+		return new NotTriangle()
+	}	
+}
